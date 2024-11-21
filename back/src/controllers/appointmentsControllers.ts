7ebb078d571b605c723fd1bc970a3f2e3196a42d
MatiasGEscobar <matiie9135@gmail.com>
2024-11-21 10:40:37 -0300

    Hito N°3 (HW) modif async

--- back/src/controllers/appointmentsControllers.ts
+++ back/src/controllers/appointmentsControllers.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from "express";
 
-export const getAppointmentsController = (req: Request, res: Response) => {
+export const getAppointmentsController = async (req: Request, res: Response) => {
     res.send("Obtener el listado de todos los turnos de todos los usuarios");
 };
 
-export const getappointmentByIdController = (req: Request, res: Response) => {
+export const getappointmentByIdController = async (req: Request, res: Response) => {
     res.send("Obtener el detalle de un turno específico");
 };
 
-export const createAppointmentController = (req: Request, res: Response) => {
+export const createAppointmentController = async (req: Request, res: Response) => {
     res.send("Agendar un nuevo turno");
 };
 
-export const cancelAppointmentController = (req: Request, res: Response) => {
+export const cancelAppointmentController = async (req: Request, res: Response) => {
     res.send("Cambiar el estatus de un turno a “cancelled”");
 };
--- back/src/controllers/usersControllers.ts
+++ back/src/controllers/usersControllers.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from "express";
 
-export const getUsersController = (req: Request, res: Response) => {
+export const getUsersController = async (req: Request, res: Response) => {
     res.send("Obtener el listado de todos los usuarios");
 };
 
-export const getUserByIdController = (req: Request, res: Response) => {
+export const getUserByIdController = async (req: Request, res: Response) => {
     res.send("Obtener el detalle de un usuario específico");
 };
 
-export const registerUserController = (req: Request, res: Response) => {
+export const registerUserController = async (req: Request, res: Response) => {
     res.send("Registro de un nuevo usuario");
 };
 
-export const loginUserController = (req: Request, res: Response) => {
+export const loginUserController = async (req: Request, res: Response) => {
     res.send("Login del usuario a la aplicación");
 };
